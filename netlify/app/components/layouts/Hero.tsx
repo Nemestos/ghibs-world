b70cfc59c3dd56472a40abeb8a2c1ad431b6cbce
turpin_l <turpin_l@etna-alternance.net>
2022-09-12 21:10:38 +0200

    [FEATURE]adding show video tool

--- netlify/app/components/layouts/Hero.tsx
+++ netlify/app/components/layouts/Hero.tsx
@@ -1,4 +1,12 @@
-import { Box, createStyles, Overlay, Title } from "@mantine/core";
+import {
+  Box,
+  Button,
+  createStyles,
+  Overlay,
+  Paper,
+  Title,
+} from "@mantine/core";
+import { useState } from "react";
 import ReactPlayer from "react-player";
 
 const useStyles = createStyles((theme, _params, getRef) => ({
@@ -18,6 +26,28 @@ const useStyles = createStyles((theme, _params, getRef) => ({
 
 export default function Hero() {
   const { classes } = useStyles();
+  const [showVideo, setShowVideo] = useState<boolean>(false);
+
+  const onShowVideo = () => {
+    setShowVideo(true);
+  };
+  if (!showVideo) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Paper>
+          <Button onClick={onShowVideo}>Show video</Button>
+        </Paper>
+      </Box>
+    );
+  }
   return (
     <Box className={classes.wrapper}>
       <Overlay opacity={0.5} zIndex={3} color="black" sx={{ height: "100%" }} />
