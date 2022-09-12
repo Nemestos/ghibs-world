e46159c694af15e1addfdc3cd9f250d9b50a9d06
turpin_l <turpin_l@etna-alternance.net>
2022-09-12 21:25:17 +0200

    [FIX]fix somme padding

--- netlify/app/components/ui/cards/MovieDetails.tsx
+++ netlify/app/components/ui/cards/MovieDetails.tsx
@@ -12,8 +12,6 @@ import {
   Button,
 } from "@mantine/core";
 import { Link } from "@remix-run/react";
-import { useColor } from "color-thief-react";
-import { useEffect } from "react";
 import type { IMovie } from "~/types";
 import Rating from "../Rating";
 
@@ -36,16 +34,20 @@ function MovieDetailsOverlay({ movie }: { movie: IMovie }) {
             justifyContent: "center",
             alignItems: "center",
 
-            gap: 128,
+            gap: 50,
           }}
         >
           <Image src={movie.image} radius="lg" width={256} />
           <Stack>
-            <Title variant="gradient">{movie.title}</Title>
-            <Title weight={"bold"}>
+            <Title variant="gradient" align="center">
+              {movie.title}
+            </Title>
+            <Title weight={"bold"} align="center">
               {movie.original_title}/{movie.original_title_romanised}
             </Title>
-            <Title italic>{movie.release_date}</Title>
+            <Title italic align="center">
+              {movie.release_date}
+            </Title>
             <Rating rating={movie.rt_score} maxStar={5} max={100} />
             <Text>{movie.description}</Text>
             <Link to={"characters"}>
@@ -59,11 +61,13 @@ function MovieDetailsOverlay({ movie }: { movie: IMovie }) {
 }
 
 export default function MovieDetails({ movie }: { movie: IMovie }) {
-  const theme = useMantineTheme();
-
   return (
     <BackgroundImage src={movie.movie_banner}>
-      <Box mx="auto" sx={{ height: "100vh", backdropFilter: "blur(5px)" }}>
+      <Box
+        mx="auto"
+        my={100}
+        sx={{ height: "100vh", backdropFilter: "blur(5px)" }}
+      >
         <MovieDetailsOverlay movie={movie} />
       </Box>
     </BackgroundImage>
